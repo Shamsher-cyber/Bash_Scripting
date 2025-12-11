788efcc5ea9bb23097f0ce94468b7add4e7c5c6f
Shamsher-cyber <devop0b111@gmail.com>
2025-12-11 11:23:07 -0500

    this is the day one in bash scripting

---
+++ day_1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+: ' --> What is echo 
+---> the echo command in bash is a built-in utility used to display a line
+of text or the value of a variable to the standard output (the terminal). 
+it is one of the most foundamental commands in shell scripting for logging information, providing user propmts, and debugging.
+  
+'
+
+echo "This is the linux script"
+
+# displaying variables using the echo and the $ sign it the command
+#  substitution to embed the output of other commands within the echo echo 
+#  statements 
+
+name='Arch linux'
+echo ""
+
+# echo "" for creating a line break
+
+echo "The $name is the most powerful linux distro"
+
+
+
+
+
+
+
+
+
+
