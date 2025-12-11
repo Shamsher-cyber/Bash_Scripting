d1121feb464cc89ac85a81ee5f2ebd5ad767b74e
Shamsher-cyber <devop0b111@gmail.com>
2025-12-11 12:15:59 -0500

    git setup guide for me

---
+++ git_setup.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+# First make github repo from the github website 
+# then follow this steps 
+#  --->   make your git directory 
+# ----->    git init 
+# ---->     git status
+# ---->        git add .  ( . for all the files or directories you have ) 
+#  ---->    git add file.txt 
+#  ------>   git commit -m  "this is git commit"
+#  
+#  -------> git branch 
+#  --------> git branch -M (name of the branch you want to rename )
+#
+#  -----------> git remote -v 
+#
+#  -------> git remote add  <paste the line you have in you gihub repo to link>
+#  
+#  ---------> make sure you link the ssh key ager nhi link tn generate new key 
+#  and add see the github documentations on that 
+#
+# ------> git remote -v 
+#
+# git push -u origin your_branch_name 
+#
+# ager code yan files push ho rahi hai tn thek hai nhi tn use this 
+#
+#  ----->> git remote set-url origin git@github.com:/your_username_ongithub/your_github_reponame.git
+#
+#  then use 
+#
+#  git push -u origin your_branch_name
+#
+#  its done !!
