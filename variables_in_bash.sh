0ea79ba42ffda0e29d40f8e5097bc33b95b2934c
Shamsher-cyber <devop0b111@gmail.com>
2025-12-15 07:37:04 -0500

    variable da tora hor code add kita

--- variables_in_bash.sh
+++ variables_in_bash.sh
@@ -103,8 +103,21 @@ echo "Return the shell type "
 echo $SHELL 
 echo ""
 
+ 
+ # printing the string values without the single 
+ # and double quotes .. ""  '' 
 
 
+var=this_is_my_name
 
+# es value ek valid string count hovegi and error bhi nhi ayega. but dikat eh hai ki snu kuj connection dina pynda hai means jive maine _ dita hai har word de last ch bakiya nu connect kita hai age esa nhi kra tn us 
+# ' ' or  " " or """ """ or ''' ''' etc ..
 
+echo $var 
 
+# we can also use the  ''' ''' triple single quotes and double triple qoutes in bash 
+
+var=''' the value in_side the Triple single  qoutes '''
+
+echo 
+echo $var
